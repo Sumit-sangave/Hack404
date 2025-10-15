e2738f4534cd17dd7bd2ba90b9f8d41196abdc72
sumit sangave <sumitsangave2631@gmail.com>
2025-10-15 07:44:35 +0530

    Resolved logs - 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // make sure base is just "/"
-})
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
